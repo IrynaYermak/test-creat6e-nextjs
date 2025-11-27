307439ceb67485d79632dad5877784c625d62517
Iryna Yermak <1991irenka@gmail.com>
2025-11-27 15:16:19 +0000

    commit

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,8 +1,50 @@
+// 'use client';
+// import { useState } from 'react';
+// import { getNotes } from '../../lib/api';
+// import type { Note } from '@/lib/api';
+// import NoteList from '@/components/NoteList/NoteList';
+
+// const Notes = () => {
+//   const [notes, setNotes] = useState<Note[]>([]);
+
+//   const handleClick = async () => {
+//     const response = await getNotes();
+//     if (response?.notes) {
+//       setNotes(response.notes);
+//     }
+//   };
+//   return (
+//     <section>
+//       <h1>Notes List</h1>
+//       <button onClick={handleClick}>Get my notes</button>
+//       {notes.length > 0 && <NoteList notes={notes} />}
+//     </section>
+//   );
+// };
+
+// export default Notes;
+
+// import { getNotes } from '@/lib/api';
+// import NoteList from '@/components/NoteList/NoteList';
+
+// const Notes = async () => {
+//   const response = await getNotes();
+
+//   return (
+//     <section>
+//       <h1>Notes List</h1>
+//       {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+//     </section>
+//   );
+// };
+
+// export default Notes;
+
 'use client';
+
 import { useState } from 'react';
-import { getNotes } from '../../lib/api';
-import type { Note } from '@/lib/api';
 import NoteList from '@/components/NoteList/NoteList';
+import { getNotes, Note } from '@/lib/api';
 
 const Notes = () => {
   const [notes, setNotes] = useState<Note[]>([]);
@@ -13,6 +55,7 @@ const Notes = () => {
       setNotes(response.notes);
     }
   };
+
   return (
     <section>
       <h1>Notes List</h1>
--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,45 @@
+// import axios from 'axios';
+
+// export type Note = {
+//   id: string;
+//   title: string;
+//   content: string;
+//   category: string;
+//   user: string;
+//   createdAt: string;
+//   updatedAt: string;
+// };
+
+// export type NoteListResponse = {
+//   notes: Note[];
+//   total: number;
+// };
+
+// const myToken = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
+// axios.defaults.baseURL = 'https://next-v1-notes-api.goit.study';
+
+// export const getNotes = async () => {
+//   console.log(myToken);
+
+//   const res = await axios.get('/notes', {
+//     headers: {
+//       Authorization: `Bearer ${myToken}`,
+//     },
+//   });
+//   return res.data;
+// };
+
+// lib/api.ts
+
 import axios from 'axios';
 
 export type Note = {
   id: string;
   title: string;
   content: string;
-  category: string;
-  user: string;
+  categoryId: string;
+  userId: string;
   createdAt: string;
   updatedAt: string;
 };
@@ -15,17 +49,9 @@ export type NoteListResponse = {
   total: number;
 };
 
-const myToken = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-
 axios.defaults.baseURL = 'https://next-v1-notes-api.goit.study';
 
 export const getNotes = async () => {
-  console.log(myToken);
-
-  const res = await axios.get<NoteListResponse>('/notes', {
-    headers: {
-      Authorization: `Bearer ${myToken}`,
-    },
-  });
+  const res = await axios.get<NoteListResponse>('/notes');
   return res.data;
 };
